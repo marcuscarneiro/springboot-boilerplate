6cd6fd2a1841baf660c1b78680446736c554de88
Marcus Carneiro <marcuscarneiro@outlook.com>
2019-10-17 19:39:54 -0300

    First commit
    Form security, in memory persistence

---
+++ src/main/java/com/marcus/project/datasource/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marcus.project.datasource;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcus.project.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
+}
